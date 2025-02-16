45299c6ce512c8062be72f6cf1ffeec39889b5c6
Gabe <54961758+gabecoatess@users.noreply.github.com>
2025-02-16 13:24:09 -0500

    fix: set `glfwSetScrollCallback` to the respective callback function

--- src/main.cpp
+++ src/main.cpp
@@ -160,6 +160,7 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // Initialize Utilities
